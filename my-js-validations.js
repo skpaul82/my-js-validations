4f69fb539e2cbfada98a21653ec226416277aed7
skpaul82 <skpaul82@gmail.com>
2018-02-17 18:33:46 +0600

    add numeric-filter and strip-tags

---
+++ my-js-validations.js
@@ -0,0 +1,22 @@
+/**
+ * JavaScript/jQuery validations that I uses for my projects
+ * @author skpaul <me@skpaul.me>
+ */
+
+/**
+ * numeric value checkup: mostly uses on mobile number and amy type number validation. 
+ * Use as a classs name "numeric-filter"
+ * Use Case: <input class="numeric-filter" name="mobile_number" />
+ */
+$('.numeric-filter').keyup(function() {
+    this.value = this.value.replace(/[^\0-9]/ig, ""); 
+});
+
+/**
+ * remove tags form input fields. Use as a classs name "strip-tags"
+ * Use Case: <input class="strip-tags" name="mobile_number" />
+ */
+//  
+$('.strip-tags').keyup(function() {
+    this.value = this.value.replace(/<\/?[^>]+(>|$)/g, "")
+});
